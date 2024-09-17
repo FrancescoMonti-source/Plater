3583a10df80a9021622e5d6f122bee3e57c7053c
FrancescoMonti-source <58150610+FrancescoMonti-source@users.noreply.github.com>
2024-09-18 00:10:30 +0200

    Create hide nameplates.lua

---
+++ hide nameplates.lua
@@ -0,0 +1,9 @@
+-- This is a script, not a mod
+-- To paste into "on show" and "on update"
+-- The trigger is defined manually in the scripting tab
+
+function (self, unitId, unitFrame, envTable)
+    -- Hide the nameplate
+    unitFrame:Hide()
+end
+
